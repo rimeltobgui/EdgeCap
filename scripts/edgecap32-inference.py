d47909fd26175f393783e13d0e83f96ddf6efad0
rimeltobgui <105317758+rimeltobgui@users.noreply.github.com>
2025-05-05 20:49:48 +0400

    Update edgecap32-inference.py

--- scripts/edgecap32-inference.py
+++ scripts/edgecap32-inference.py
@@ -86,7 +86,8 @@ image_transform = transforms.Compose([
 ])
 
 # === Load trained weights for the MLP mapper ===
-model.mapper.load_state_dict(torch.load("weights/edgecap32-mapper_trained_25epochs.pth", map_location=device))
+model.mapper.load_state_dict(torch.load("weights/edgecap32-mapper_trained_25epochs.pth", map_location=device, weights_only=False))
+
 
 # === Generate caption using beam search and transformer decoding ===
 @torch.no_grad()
